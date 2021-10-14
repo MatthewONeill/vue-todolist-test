994654bfe2aa03a5ca5544e5fe4942d6689b0c74
Matthew <matthewjo30@gmail.com>
2021-10-13 22:59:29 -0400

    init

---
+++ src/components/TodoList.vue
@@ -0,0 +1,63 @@
+<template>
+    <input type="text" class="border border-black" v-model="todoText">
+    <button class="border border-black ml-6" v-on:click="addTodo">Test</button>
+
+    <ul v-if="todos.length">
+        <Todo 
+            v-for="todo in todos" 
+            :key="todo.id"
+            :todo="todo"
+            @remove="removeTodo"
+        />
+    </ul>
+    <p v-else>
+        Nothing here.
+    </p>
+    
+</template>
+
+
+<script>
+import Todo from './Todo.vue'
+
+let todoId = 1;
+
+export default {
+    components: {
+        Todo
+    },
+    name: 'TodoList',
+    props: {
+    },
+
+    data () {
+        return {
+            todos: [
+                {
+                    id: 0,
+                    text: "test"
+                }
+            ],
+            todoText: ''
+        }
+    },
+
+    methods: {
+        addTodo: function () {
+            this.todos.push({
+                id: todoId++,
+                text: this.todoText
+            })
+        },
+        removeTodo (id) {
+            this.todos = this.todos.filter(todo => {
+                return todo.id !== id
+            });
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
